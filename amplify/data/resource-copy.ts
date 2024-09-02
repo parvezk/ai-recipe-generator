d4ffde0eb593d9ff1e6cb1d95818dcf461b5c608
Parvez Kose <parvez.kosex@gmail.com>
2024-09-02 16:16:58 -0400

    bedrock connection

--- amplify/data/resource-copy.ts
+++ amplify/data/resource-copy.ts
@@ -6,21 +6,12 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
-
 const schema = a.schema({
-  BedrockResponse: a.customType({
-    body: a.string(),
-    error: a.string(),
-  }),
-
-  askBedrock: a
-    .query()
-    .arguments({ ingredients: a.string().array() })
-    .returns(a.ref("BedrockResponse"))
-    .authorization((allow) => [allow.authenticated()])
-    .handler(
-      a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
-    ),
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -28,10 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: "iam",
   },
 });
 
